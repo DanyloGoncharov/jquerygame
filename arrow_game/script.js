13ce894c4aefafda84a59b1380c7fec06464bcbd
DanyloHoncharov <danil3131@ukr.net>
2020-02-18 15:59:22 +0200

    aditional function

--- arrow_game/script.js
+++ arrow_game/script.js
@@ -32,27 +32,68 @@ const start_top =parseInt($(".square").css("top"),10)
 let nextleft = parseInt($(".square").css("left"),10);
 let nexttop = parseInt($(".square").css("top"),10);
 console.log(start_left , start_top)
-let agree = confirm("Do you want to play this game")
 document.addEventListener("keydown", game, false);
-
-function game(k) {
-console.log(k.keyCode)
-if (k.keyCode == 37 ) {
+function key(e) {
+    console.log(e.keyCode)
+}
+$(".left").click(function(){
     $(".square").css("left", nextleft-5);
     console.log("The square is going to left")
-    nextleft = nextleft-5
+    nextleft = nextleft-5;
+})
+$(".up").click(function(){
+    $(".square").css("top", nexttop-5);
+    console.log("The square is going to top")
+    nexttop = nexttop-5
+})
+$(".right").click(function(){
+    $(".square").css("left", nextleft+5);
+    console.log("The square is going to right");
+    nextleft = nextleft+5
+})
+$(".down").click(function(){
+    $(".square").css("top" , nexttop + 5);
+    console.log("The square is going to bottom")
+    nexttop = nexttop+5
+})
+$(".left").hover(function(){
+    $(".square").css("left", nextleft-5);
+    console.log("The square is going to left")
+    nextleft = nextleft-5;
+})
+$(".up").hover(function(){
+    $(".square").css("top", nexttop-5);
+    console.log("The square is going to top")
+    nexttop = nexttop-5
+})
+$(".right").hover(function(){
+    $(".square").css("left", nextleft+5);
+    console.log("The square is going to right");
+    nextleft = nextleft+5
+})
+$(".down").hover(function(){
+    $(".square").css("top" , nexttop + 5);
+    console.log("The square is going to bottom")
+    nexttop = nexttop+5
+})
+function game(k) {
+console.log(k.keyCode)
+if (k.keyCode == 37|| k.keyCode == 65 )  {
+    $(".square").css("left", nextleft-5);
+    console.log("The square is going to left")
+    nextleft = nextleft-5;
 }
- if (k.keyCode == 38) { 
+ if (k.keyCode == 38 || k.keyCode == 87 ) { 
     $(".square").css("top", nexttop-5);
     console.log("The square is going to top")
     nexttop = nexttop-5
 }
- if (k.keyCode == 39) {
+ if (k.keyCode == 39 || k.keyCode == 68) {
     $(".square").css("left", nextleft+5);
     console.log("The square is going to right");
     nextleft = nextleft+5
 }
- if (k.keyCode == 40 ) {
+ if (k.keyCode == 40 || k.keyCode == 83) {
     $(".square").css("top" , nexttop + 5);
     console.log("The square is going to bottom")
     nexttop = nexttop+5
