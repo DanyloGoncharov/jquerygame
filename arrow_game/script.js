4ef23e860873b02ccd0831f4cc820ef60a030788
DanyloHoncharov <danil3131@ukr.net>
2020-02-22 20:28:26 +0200

    pula add

--- arrow_game/script.js
+++ arrow_game/script.js
@@ -1,34 +1,7 @@
-// document.addEventListener("keydown", key, false);
-// function key(e) {
-//     console.log(e.keyCode)
-// }
-// const start_left =parseInt($(".square").css("left"),10)
-// let height = document.documentElement.clientHeight;
-// let width = document.documentElement.clientWidth; //
-// document.addEventListener("keydown", game, false);
-// function left() {
-    //     $(".square").css("left", start_left-5)
-    // }
-// function game() {
-    //     function play(k) {
-        
-        //         document.addEventListener("keydown", game, false);
-        //     console.log(k.keyCode)
-//     if (k.keyCode == 37) {
-    //         left();
-    
-    //     }
-    // }
-    // play();
-    // }
-    // while (agree = true) {game() }
-
-    
-    
+let btn;
+let pula = $("<div class='pula'></div>")    
 const start_left =parseInt($(".square").css("left"),10)
 const start_top =parseInt($(".square").css("top"),10)
-// let height = document.documentElement.clientHeight;
-// let width = document.documentElement.clientWidth;
 let nextleft = parseInt($(".square").css("left"),10);
 let nexttop = parseInt($(".square").css("top"),10);
 console.log(start_left , start_top)
@@ -82,21 +55,89 @@ if (k.keyCode == 37|| k.keyCode == 65 )  {
     $(".square").css("left", nextleft-5);
     console.log("The square is going to left")
     nextleft = nextleft-5;
+    btn = 37
 }
  if (k.keyCode == 38 || k.keyCode == 87 ) { 
     $(".square").css("top", nexttop-5);
     console.log("The square is going to top")
     nexttop = nexttop-5
+    btn = 38
 }
  if (k.keyCode == 39 || k.keyCode == 68) {
     $(".square").css("left", nextleft+5);
     console.log("The square is going to right");
     nextleft = nextleft+5
+    btn = 39
 }
  if (k.keyCode == 40 || k.keyCode == 83) {
     $(".square").css("top" , nexttop + 5);
     console.log("The square is going to bottom")
     nexttop = nexttop+5
+    btn = 40
+}
+if ( btn == 37 || btn == null){
+
+    if (k.keyCode == 32) {
+        let pulatop = nexttop-27;
+        let pulaleft = nextleft-30;
+        pula.css("top", pulatop);
+        pula.css("left", pulaleft);
+        $("body").append(pula)
+        if(pula.css("left") !== 0) {
+            pula.animate({
+                "opacity" : 1,
+                "left" : 1
+            })
+        }
+    }
+}
+if ( btn == 40){
+
+    if (k.keyCode == 32) {
+        let pulatop = nexttop-27;
+        let pulaleft = nextleft-30;
+        pula.css("top", pulatop);
+        pula.css("left", pulaleft);
+        $("body").append(pula)
+        if(pula.css("left") !== 0) {
+            pula.animate({
+                "opacity" : 1,
+                "top" : document.documentElement.clientHeight
+            })
+        }
+    }
+}
+if ( btn == 38){
+
+    if (k.keyCode == 32) {
+        let pulatop = nexttop-27;
+        let pulaleft = nextleft-30;
+        pula.css("top", pulatop);
+        pula.css("left", pulaleft);
+        $("body").append(pula)
+        if(pula.css("left") !== 0) {
+            pula.animate({
+                "opacity" : 1,
+                "top" : 1
+            })
+        }
+    }
+}
+if ( btn == 39){
+
+    if (k.keyCode == 32) {
+        let pulatop = nexttop-27;
+        let pulaleft = nextleft-10;
+        pula.css("top", pulatop);
+        pula.css("left", pulaleft);
+        $("body").append(pula)
+        if(pula.css("left") !== 0) {
+            pula.animate({
+                "opacity" : 1,
+                "left" : document.documentElement.clientWidth
+            })
+        }
+    }
 }
 }
 $(".start").click( function(){
